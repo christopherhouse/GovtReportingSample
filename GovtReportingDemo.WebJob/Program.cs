bfeb90d977abc3974a32d8b892b38e2ef3d097c9
Christopher House <chhouse@microsoft.com>
2021-07-15 10:46:41 -0500

    initial add of webjob console app

---
+++ GovtReportingDemo.WebJob/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GovtReportingDemo.WebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+
+            builder.ConfigureWebJobs(_ =>
+            {
+                _.AddAzureStorageCoreServices();
+                _.AddAzureStorage();
+            });
+
+            builder.ConfigureLogging((ctx, _) =>
+            {
+                _.AddConsole();
+
+                var appInsightsKey = ctx.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+
+                if (!string.IsNullOrWhiteSpace(appInsightsKey))
+                {
+                    _.AddApplicationInsightsWebJobs(ai => ai.InstrumentationKey = appInsightsKey);
+                }
+            });
+
+#if DEBUG
+            builder.UseEnvironment("development");
+#endif
+
+            var host = builder.Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
